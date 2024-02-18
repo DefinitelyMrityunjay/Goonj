6a546ea32e6ec462c0b70c8163655e7f7b43d6e3
Hitesh Bandhu <bandhuhitesh52003@gmail.com>
2024-02-18 17:02:27 +0530

    updated star night page and made it responsive

--- src/pages/artist.tsx
+++ src/pages/artist.tsx
@@ -15,7 +15,7 @@ export default function Artist() {
         <header>
           <Navbar color={"#AD0000"} />
         </header>
-        <div className={styles.artist}>
+        <div className={styles.artist} style={{display:"grid", placeItems:"center" }}>
           <h2>Kulwinder Billa</h2>
           <div className={styles.media}>
             <video
@@ -32,11 +32,12 @@ export default function Artist() {
               height={1080}
             />
           </div>
+          <div className={styles.preArtist} style={{display:"grid", placeItems:"center" }}>
           <p>Our Previous Star</p>
-          <h3>Gurnaam Bullar</h3>
-          <div className={styles.preArtist}>
-            <img
-              className={styles.img}
+          <h3 style={{marginTop:"4rem", marginBottom:"-30px"}}>Gurnaam Bullar</h3>
+            <Image
+              className={styles.imgb}
+              style={{ width: "60%", height: "auto" }}
               src="/artist/gurnaam.png"
               alt="Gurnaam Bullar"
               width={700}
